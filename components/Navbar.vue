e91ed3352602bc04fdaf4347a733d23efe344a94
Amir <37193793+leVenour@users.noreply.github.com>
2023-12-24 06:46:22 +0100

    Update components/Navbar.vue
    Co-authored-by: Zoey <zoeykaiser8@gmail.com>

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@ const { data, status, signIn } = useAuth()
 
 <template>
   <div class="flex w-full px-3 shadow-lg bg-gradient-to-r from-blue-300 to-blue-400 h-16 items-center justify-end">
-    <p v-if="status === 'authenticated'" class="flex items-center justify-center gap-2">
+    <p v-if="status === 'authenticated' && data" class="flex items-center justify-center gap-2">
       {{ data.user.name }} <Icon class="rounded-full" name="mdi:user" size="1.5rem" />
     </p>
     <button v-else class="flex justify-center items-center h-full" @click="signIn('keycloak')">
