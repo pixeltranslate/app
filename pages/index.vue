2d4f9114b1f30f52ea52682febaf44bac695bdb2
Amir <37193793+leVenour@users.noreply.github.com>
2023-12-24 06:47:26 +0100

    feat: Add Navbar (#4)
    Co-authored-by: Zoey <zoeykaiser8@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -7,19 +7,22 @@ const isLoggedIn = computed(() => {
 </script>
 
 <template>
-  <div class="max-w-2xl mx-auto my-5 text-center">
-    <div class="flex items-center gap-2 justify-center">
-      <button :disabled="isLoggedIn" @click="signIn('keycloak')">
-        Login
-      </button>
-      <button :disabled="!isLoggedIn" @click="signOut()">
-        Logout
-      </button>
-    </div>
-    <hr class="my-4">
-    status: {{ status }}
-    <pre v-if="data" wrap class="bg-black p-2 text-white rounded mt-2 text-left">
+  <div>
+    <Navbar />
+    <div class="max-w-2xl mx-auto my-5 text-center">
+      <div class="flex items-center gap-2 justify-center">
+        <button :disabled="isLoggedIn" @click="signIn('keycloak')">
+          Login
+        </button>
+        <button :disabled="!isLoggedIn" @click="signOut()">
+          Logout
+        </button>
+      </div>
+      <hr class="my-4">
+      status: {{ status }}
+      <pre v-if="data" wrap class="bg-black p-2 text-white rounded mt-2 text-left">
       {{ JSON.stringify(data, null, 2) }}
     </pre>
+    </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        pixel: {
+          orange: '#F0903B',
+          blue: '#4897DF'
+        }
+      }
+    }
+  }
+}
