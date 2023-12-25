b979675664a49120a6c8391bee1128308f3b39d3
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-12-25 17:55:25 +0100

    added auth middleware

--- components/Navigation/Bar.vue
+++ components/Navigation/Bar.vue
@@ -3,6 +3,10 @@ const { username, avatar } = useUser()
 const { signIn, signOut, status } = useAuth()
 const { toggle: toggleSidebar } = useSidebar()
 
+defineProps<{
+  landingView?: boolean
+}>()
+
 const items = [
   [{
     label: 'Profile',
@@ -10,6 +14,7 @@ const items = [
   }], [{
     label: 'Logout',
     icon: 'i-pixelarticons-logout',
+
     click: signOut
   }]
 ]
@@ -19,7 +24,7 @@ const items = [
   <div class="flex h-20 items-center justify-between pl-2 pr-8 border-b border-border">
     <div class="flex items-center gap-2">
       <div class="flex">
-        <div class="flex w-9 h-9 flex items-center justify-center rounded cursor-pointer hover:bg-foreground" @click="toggleSidebar">
+        <div v-if="!landingView" class="flex w-9 h-9 flex items-center justify-center rounded cursor-pointer hover:bg-foreground" @click="toggleSidebar">
           <Icon
             name="pixelarticons:menu"
             size="25px"
@@ -33,15 +38,34 @@ const items = [
         </h1>
       </div>
     </div>
-    <UDropdown v-if="status === 'authenticated'" :items="items" :popper="{ placement: 'bottom-start' }">
-      <div class="flex items-center gap-2 text-sm">
-        <span class="capitalize">{{ username }}</span>
-        <UAvatar icon="i-heroicons-photo" size="sm" :text="avatar.text" :src="avatar.src" />
-      </div>
-    </UDropdown>
-    <UButton v-else variant="ghost" size="lg" color="gray" @click="signIn('keycloak')">
-      Sign In
-    </UButton>
+    <div v-if="status === 'authenticated'" class="flex items-center gap-3">
+      <UButton
+        v-if="landingView"
+        to="/app"
+        target="_blank"
+        variant="solid"
+        size="lg"
+        color="gray"
+        @click="signIn('keycloak')"
+      >
+        Visit App
+        <Icon name="pixelarticons:arrow-right" size="20" />
+      </UButton>
+      <UDropdown v-else :items="items" :popper="{ placement: 'bottom-start' }">
+        <div class="flex items-center gap-2 text-sm">
+          <span class="capitalize">{{ username }}</span>
+          <UAvatar icon="i-heroicons-photo" size="sm" :text="avatar.text" :src="avatar.src" />
+        </div>
+      </UDropdown>
+    </div>
+    <div v-else class="flex items-center gap-3">
+      <UButton variant="solid" size="lg" color="primary" @click="signIn('keycloak')">
+        Login
+      </UButton>
+      <UButton variant="solid" size="lg" color="white" @click="signIn('keycloak')">
+        Create account
+      </UButton>
+    </div>
   </div>
 </template>
 
---
+++ pages/auth/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { signIn } = useAuth()
+
+onMounted(() => {
+  signIn('keycloak')
+})
+
+definePageMeta({
+  layout: 'none',
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <TheLoader />
+  </div>
+</template>
