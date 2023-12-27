262ed5edca2eacd557d7a9101ed8f6ae17a4ad33
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-12-27 19:41:11 +0100

    fixed line

--- middleware/session-timeout.global.ts
+++ middleware/session-timeout.global.ts
@@ -1,8 +1,8 @@
 export default defineNuxtRouteMiddleware(() => {
   const { data, signOut } = useAuth()
 
-  if (data.value?.expires) {
   // See when the session is supposed to expire and if that point has passed, force logout the user
+  if (data.value?.expires) {
     const date = new Date(data.value?.expires)
     if (new Date() >= date) {
       signOut()
