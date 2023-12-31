477964468be23b17ed59eb5ba700ce8f81874c99
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-12-31 19:28:12 +0100

    refactored v1 of table

--- pages/workspace/[workspaceId]/project/[projectId]/collection/[collectionId].vue
+++ pages/workspace/[workspaceId]/project/[projectId]/collection/[collectionId].vue
@@ -66,45 +66,43 @@ const addItem = () => {
     </template>
     <UCard :ui="{ body: { padding: '!p-0' }, rounded: 'rounded' }">
       <div class="relative overflow-x-scroll h-[79vh]">
-        <table class="text-sm text-left relative">
-          <thead class="sticky top-0 text-sm z-10 bg-primary dark:bg-primary-dark text-white">
-            <tr>
-              <th scope="col">
-                <div class="p-2 border-r border-primary-light dark:border-primary-light/30">
+        <div class="text-sm text-left relative">
+          <div class="sticky top-0 text-sm z-10 flex flex-col w-full">
+            <div class="flex items-center w-full">
+              <div scope="row" class="flex">
+                <div class="w-[300px] px-4 py-3">
                   Key
                 </div>
-              </th>
-              <th v-for="lang in selectedLanguages" :key="lang" scope="col">
-                <div class="p-3 border-r border-primary-light dark:border-primary-light/30">
+              </div>
+              <div v-for="lang in selectedLanguages" :key="lang" class="min-w-[300px] w-full">
+                <div class="p-3">
                   {{ lang }}
                 </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in filteredItems" :key="item.id">
-              <th scope="row">
-                <div class="min-w-[300px] border-b border-r dark:border-border font-normal">
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div v-for="item in filteredItems" :key="item.id" class="flex items-center w-full">
+              <div scope="row" class="flex">
+                <div class="w-[300px] border-b border-r dark:border-border font-normal">
                   <UInput :value="item.name" variant="none" :ui="{ rounded: 'rounded-none' }" />
                 </div>
-              </th>
-              <td v-for="translation in item.translations" :key="translation" class="p-0">
-                <div class="min-w-[300px] border-r border-b dark:border-border">
-                  <UInput :placeholder="translation" variant="none" :ui="{ rounded: 'rounded-none' }" />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row" :colspan="selectedLanguages.length + 1">
+              </div>
+              <div v-for="translation in item.translations" :key="translation" class="min-w-[300px] w-full border-r border-b dark:border-border">
+                <UInput :placeholder="translation" variant="none" :ui="{ rounded: 'rounded-none' }" />
+              </div>
+            </div>
+            <div>
+              <div scope="row" :colspan="selectedLanguages.length + 1">
                 <div class="relative py-2 hover:bg-gray-100 dark:hover:bg-foreground cursor-pointer mt-[-2px]" @click="addItem">
                   <div class="sticky left-0 px-4 w-[300px] text-sm font-normal text-gray-500 dark:text-gray-400">
                     + Add another row
                   </div>
                 </div>
-              </th>
-            </tr>
-          </tbody>
-        </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </UCard>
   </TheLayout>
