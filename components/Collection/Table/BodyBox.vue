fac8287e3298b48749c2a32bf40fd315c5d5abd0
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-01-01 15:28:15 +0100

    final nice updates

--- components/Collection/Table/BodyBox.vue
+++ components/Collection/Table/BodyBox.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
-const props = defineProps<{ defaultValue?: string }>()
+const props = defineProps<{ defaultValue?: string, rowId: string }>()
 const emit = defineEmits<{(e: 'update', value: string): string}>()
 
+const { selectedLanguages } = useCollectionTable()
+
 const value = ref(props.defaultValue)
+const target = ref()
+
+const width = computed(() => {
+  return (1 / selectedLanguages.value.length) * 100
+})
 
 const onType = (input: string) => {
   if (input === value.value) {
@@ -12,16 +19,15 @@ const onType = (input: string) => {
 </script>
 
 <template>
-  <td class="border-r w-full min-w-[400px] dark:border-border bg-gray-100 dark:bg-transparent">
-    <UTextarea
+  <td class="relative border-r min-w-[400px] w-full dark:border-border bg-gray-100 dark:bg-transparent" :style="`max-width: ${width}%`">
+    <textarea
       ref="target"
       v-model="value"
       v-debounce:1s="onType"
-      :debounce-events="['keydown']"
+      class="absolute w-full h-full top-0 left-0 py-1.5 px-2.5 bg-transparent outline-none resize-none focus-visible:ring-2 ring-white/20"
       variant="none"
-      resize
-      placeholder="Enter your value..."
-      :ui="{ base: 'min-h-[100px]' }"
+      placeholder="Enter your translation..."
+      :debounce-events="['keydown']"
     />
   </td>
 </template>
--- components/Collection/Table/index.vue
+++ components/Collection/Table/index.vue
@@ -13,7 +13,7 @@ const addItem = () => {
         <thead>
           <tr class="bg-gray-50 dark:bg-foreground border-b dark:border-border">
             <th class="border-r p-2 dark:border-border" />
-            <CollectionTableHeaderBox label="Key" width="min-w-[300px]" />
+            <CollectionTableHeaderBox label="Key" width="w-[300px]" />
             <template
               v-for="language in languageOptions"
               :key="language"
@@ -32,20 +32,35 @@ const addItem = () => {
             class="bg-gray-100 dark:bg-transparent text-center border-b text-sm dark:border-border"
           >
             <td class="p-2 border-r dark:border-border">
-              <UButton square color="red" variant="outline">
-                <Icon name="pixelarticons:minus" size="15" />
-              </UButton>
+              <div class="flex flex-col gap-2">
+                <UTooltip text="Show info" :popper="{ arrow: true, placement: 'top' }">
+                  <UButton square color="primary" variant="soft">
+                    <Icon name="entypo:popup" size="15" />
+                  </UButton>
+                </UTooltip>
+                <UTooltip text="Delete entry" :popper="{ arrow: true }">
+                  <UButton square color="red" variant="soft">
+                    <Icon name="material-symbols:delete-outline" size="15" />
+                  </UButton>
+                </UTooltip>
+              </div>
             </td>
-            <td class="border-r w-full min-w-[300px] dark:border-border">
-              <UInput
+            <td
+              class="border-r min-w-[300px] dark:border-border bg-gray-200/40 dark:bg-foreground/40"
+              :class="selectedLanguages.length === 0 && 'w-full'"
+            >
+              <UTextarea
                 v-model="item.name"
                 variant="none"
                 placeholder="Identifier"
+                resize
+                :ui="{ base: 'min-h-[100px]' }"
               />
             </td>
             <template v-for="(translation, index) in item.translations" :key="translation">
               <CollectionTableBodyBox
                 v-if="selectedLanguages.includes(index)"
+                :row-id="item.id"
                 :default-value="translation"
                 @update="(value) => updateEntry(value, item.id, index)"
               />
