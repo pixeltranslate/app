fd328700bc4485bcc1425b2007e939fc1a86e68a
leVenour <leVenour@gmx.at>
2024-01-06 02:08:30 +0100

    fix: Redesigned the collections site

--- pages/workspace/[workspaceId]/project/[projectId]/collection/index.vue
+++ pages/workspace/[workspaceId]/project/[projectId]/collection/index.vue
@@ -9,12 +9,6 @@ const { data, isLoading } = collectionQuery.all({
   projectId
 })
 
-const columns = [{
-  key: 'name',
-  label: 'Name',
-  class: 'w-full'
-}, { key: 'actions' }]
-
 const items = (row: CollectionGetAll) => [
   [{
     label: 'Edit',
@@ -46,6 +40,30 @@ const items = (row: CollectionGetAll) => [
         @click="collectionCreateOrEdit.open({ mode: 'create', data: { workspaceId, projectId } })"
       />
     </template>
+    <TheLoader v-if="isLoading" />
+    <div v-else>
+      <div v-for="value in data" :key="value.id" class="grid grid-cols-2 items-center px-3 py-2 my-2 bg-gray-200 dark:bg-foreground rounded-md shadow-sm border-border border-opacity-20 dark:border-border border-[1px]">
+        <p>
+          <Icon name="uis:grid" class="mb-[2px]" color="gray" />
+          {{ value.name }}
+          <span class="flex items-center text-[12px]">
+            <span v-if="value.entries">{{ value.entries }} <span v-if="value.entries == 1">entry</span><span v-else>entries</span></span>
+            <span v-else>No entries</span>
+          </span>
+        </p>
+        <div class="flex justify-end">
+          <UButtonGroup size="sm" orientation="horizontal">
+            <UButton :to="`./collection/${value.id}`">
+              <Icon name="carbon:view" class="text-black dark:text-white" /> View
+            </UButton>
+            <UDropdown :items="items(value)" :popper="{ placement: 'bottom-end' }">
+              <UButton color="white" trailing-icon="i-pixelarticons-chevron-down" />
+            </UDropdown>
+          </UButtonGroup>
+        </div>
+      </div>
+    </div>
+    <!--
     <UCard :ui="{ body: { padding: '!p-2' } }">
       <UTable
         :columns="columns"
@@ -63,5 +81,6 @@ const items = (row: CollectionGetAll) => [
         </template>
       </UTable>
     </UCard>
+    -->
   </TheLayout>
 </template>
