db04bc76b75a3b5a1abf98b3cfbc9ec0667b50de
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-01-06 02:00:19 +0100

    added link support

--- pages/workspace/[workspaceId]/project/[projectId]/index.vue
+++ pages/workspace/[workspaceId]/project/[projectId]/index.vue
@@ -3,10 +3,9 @@ const { project, workspaceId } = usePage()
 const { projectCreateOrEdit } = useGlobalOpeners()
 
 const actions = [
-
   {
     title: 'Add your first collection',
-    href: '#',
+    href: './collection',
     icon: 'octicon:file-directory-symlink-16',
     iconForeground: 'text-teal-700 dark:text-teal-50',
     iconBackground: 'bg-teal-50 dark:bg-teal-700'
@@ -33,7 +32,6 @@ const actions = [
     iconBackground: 'bg-yellow-50 dark:bg-yellow-700'
   }
 ]
-
 </script>
 
 <template>
@@ -56,32 +54,32 @@ const actions = [
 
     <div>
       <div class="divide-y divide-gray-200 dark:divide-border overflow-hidden sm:grid sm:grid-cols-4 sm:gap-px sm:divide-y-0">
-        <div
+        <UButton
           v-for="action in actions"
           :key="action.title"
-          class="group relative bg-white dark:bg-foreground p-4 hover:ring-2 hover:ring-inset hover:ring-secondary/50 first:rounded-l last:rounded-r"
+          :padded="false"
+          :to="action.href"
+          variant="ghost"
         >
-          <div>
-            <span :class="[action.iconBackground, action.iconForeground, 'inline-flex rounded-lg p-3 ring-0']">
-              <Icon :name="action.icon" size="20px" />
+          <div class="group relative bg-white dark:bg-foreground p-5 hover:ring-2 hover:ring-inset hover:ring-secondary/50 text-left">
+            <div>
+              <span :class="[action.iconBackground, action.iconForeground, 'inline-flex rounded-lg p-3 ring-0']">
+                <Icon :name="action.icon" size="20px" />
+              </span>
+            </div>
+            <div class="mt-4">
+              <h3 class="leading-6 text-gray-900 dark:text-gray-200 text-sm">
+                {{ action.title }}
+              </h3>
+              <p class="mt-1 text-sm text-gray-500 dark:text-gray-400 text-xs">
+                Doloribus dolores nostrum quia qui natus officia quod et dolorem.
+              </p>
+            </div>
+            <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400 group-hover:text-gray-100" aria-hidden="true">
+              <Icon name="ph:arrow-up-right-bold" size="24px" />
             </span>
           </div>
-          <div class="mt-4">
-            <h3 class="leading-6 text-gray-900 dark:text-gray-200 text-sm">
-              <a :href="action.href" class="focus:outline-none">
-                <!-- Extend touch target to entire panel -->
-                <span class="absolute inset-0" aria-hidden="true" />
-                {{ action.title }}
-              </a>
-            </h3>
-            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400 text-xs">
-              Doloribus dolores nostrum quia qui natus officia quod et dolorem.
-            </p>
-          </div>
-          <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400 group-hover:text-gray-100" aria-hidden="true">
-            <Icon name="ph:arrow-up-right-bold" size="24px" />
-          </span>
-        </div>
+        </UButton>
       </div>
     </div>
   </TheLayout>
