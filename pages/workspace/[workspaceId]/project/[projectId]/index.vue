d9169111d64de45dcf019dec7056e9e1dcaa0e3b
Zoey <zoeykaiser8@gmail.com>
2024-01-08 18:47:33 +0100

    feat: Improve responsiveness (#36)

--- pages/workspace/[workspaceId]/project/[projectId]/index.vue
+++ pages/workspace/[workspaceId]/project/[projectId]/index.vue
@@ -52,50 +52,20 @@ const actions = [
       />
     </template>
 
-    <div class="flex flex-col gap-3">
-      <div class="divide-y divide-gray-200 dark:divide-border overflow-hidden sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-px sm:divide-y-0">
-        <UButton
-          v-for="action in actions"
-          :key="action.title"
-          :padded="false"
-          :to="action.href"
-          variant="ghost"
-          class="w-full"
-        >
-          <div class="group relative bg-white dark:bg-foreground p-5 hover:ring-2 hover:ring-inset hover:ring-secondary/50 text-left">
-            <div>
-              <span :class="[action.iconBackground, action.iconForeground, 'inline-flex rounded-lg p-3 ring-0']">
-                <Icon :name="action.icon" size="20px" />
-              </span>
-            </div>
-            <div class="mt-4">
-              <h3 class="leading-6 text-gray-900 dark:text-gray-200 text-sm">
-                {{ action.title }}
-              </h3>
-              <p class="mt-1 text-sm text-gray-500 dark:text-gray-400 text-xs">
-                Doloribus dolores nostrum quia qui natus officia quod et dolorem.
-              </p>
-            </div>
-            <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400 group-hover:text-gray-100" aria-hidden="true">
-              <Icon name="ph:arrow-up-right-bold" size="24px" />
-            </span>
-          </div>
-        </UButton>
-      </div>
+    <DashboardCards :actions="actions" />
 
-      <div class="grid xl:grid-cols-6 gap-3">
-        <div class="xl:col-span-4">
-          <TheContentPlaceholder
-            label="No integrations added yet"
-            description="Integrations allow you to sync your translations in your code editors."
-          />
-        </div>
-        <div class="xl:col-span-2">
-          <TheContentPlaceholder
-            label="No project members yet."
-            description="Add you first teammate soon!"
-          />
-        </div>
+    <div class="grid xl:grid-cols-6 gap-3 mt-3">
+      <div class="xl:col-span-4">
+        <TheContentPlaceholder
+          label="No integrations added yet"
+          description="Integrations allow you to sync your translations in your code editors."
+        />
+      </div>
+      <div class="xl:col-span-2">
+        <TheContentPlaceholder
+          label="No project members yet."
+          description="Add you first teammate soon!"
+        />
       </div>
     </div>
   </TheLayout>
